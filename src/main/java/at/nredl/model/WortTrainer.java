307816e7b64a71d84478813ca7e89d64141dae7c
Nikolaus Redl <nredl@student.tgm.ac.at>
2024-10-06 00:21:05 +0200

    WortTrainerklasse erstellt

--- src/main/java/at/nredl/model/WortTrainer.java
+++ src/main/java/at/nredl/model/WortTrainer.java
@@ -1,4 +1,62 @@
 package at.nredl.model;
 
+import java.util.List;
+
 public class WortTrainer {
+
+    private List<Wort> wordList;
+    private int listIndex;
+    private int correctGuessCount;
+    private int totalGuessCount;
+
+    public WortTrainer() {
+
+    }
+
+    public void setWordList(List<Wort> wordList) {
+        if(wordList.isEmpty()) {
+            throw new IllegalArgumentException("Wortliste darf nicht leer sein.");
+        }
+        this.wordList = wordList;
+    }
+
+    public List<Wort> getWordList() {
+        return this.wordList;
+    }
+
+    public void setCorrectGuessCount(int correctGuessCount) {
+        if(correctGuessCount < 0) {
+            throw new IllegalArgumentException("CorrectGuessCount darf nicht niedriger als 0 sein.");
+        }
+        this.correctGuessCount = correctGuessCount;
+    }
+    public int getCorrectGuessCount() {
+        return this.correctGuessCount;
+    }
+
+    public void setTotalGuessCount(int totalGuessCount) {
+        if(totalGuessCount < 0) {
+            throw new IllegalArgumentException("TotalGuessCount darf nicht niedriger als 0 sein.");
+        }
+        this.totalGuessCount = totalGuessCount;
+    }
+
+    public int getTotalGuessCount() {
+        return this.totalGuessCount;
+    }
+
+    public Wort currentWord() {
+        return wordList.get(listIndex);
+    }
+
+    public boolean check(String input) {
+       totalGuessCount++;
+       boolean b = input.equalsIgnoreCase(currentWord().getWort());
+       if(b) {
+           correctGuessCount++;
+       }
+       return b;
+    }
+
+
 }
