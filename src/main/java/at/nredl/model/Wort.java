d6bc9485886159943adb8332e5293c7f8e0f61d8
Nikolaus Redl <nredl@student.tgm.ac.at>
2024-10-05 20:50:51 +0200

    Wortklasse erstellt

--- src/main/java/at/nredl/model/Wort.java
+++ src/main/java/at/nredl/model/Wort.java
@@ -1,4 +1,41 @@
 package at.nredl.model;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Wort {
+
+    private String bezeichnung;
+    private String url;
+
+    public Wort(String bezeichnung, String url) {
+
+    }
+
+    public void setBezeichnung(String bezeichnung) {
+        if(bezeichnung.isEmpty()) {
+            throw new IllegalArgumentException("Das Wort darf nicht leer sein.");
+        }
+        this.bezeichnung = bezeichnung;
+    }
+
+    public String getWort() {
+        return this.bezeichnung;
+    }
+
+    public void setUrl(String url) {
+        if(url.isEmpty()) {
+            throw new IllegalArgumentException("Die Url darf nicht leer sein.");
+        }
+        try {
+            URL tempUrl = new URL(url);
+            this.url = url;
+        } catch (MalformedURLException mue) {
+            mue.printStackTrace();
+        }
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
 }
